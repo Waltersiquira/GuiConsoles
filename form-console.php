15dfcecd9615a409fa0f51e7a371ec0c63d30298
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-29 21:59:44 -0300

    Create form-console.php

---
+++ form-console.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiConsoles</title>
+</head>
+<body>
+    <form action="enviar-form-console.php" method="post">
+        digite o nome do console <br>
+        <input type="text" name="nome"> <br>
+        digite as especificações do console <br>
+        <input type="text" name="especificacoes"> <br>
+        digite o preço do console <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem do console <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
